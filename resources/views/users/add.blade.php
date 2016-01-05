0dea4a929a40f5be70ebb35465f416bb74860fe9
jhassan <jawedjee0519@gmail.com>
2016-01-05 10:28:57 +0500

    Change the layout of User

--- resources/views/users/add.blade.php
+++ resources/views/users/add.blade.php
@@ -366,101 +366,76 @@
           <!-- Default box -->
           <div class="box">
             <div class="box-header with-border">
-              <h3 class="box-title">Title</h3>
+              <h3 class="box-title">User</h3>
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
                 <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
               </div>
             </div>
-            <div class="box-body">
-            	<!-- errors -->
-                    <div class="has-error">
+            
+             <div class="box box-primary">
+             	<div class="has-error">
                         {!! $errors->first('first_name', '<span class="help-block">:message</span>') !!}
                         {!! $errors->first('last_name', '<span class="help-block">:message</span>') !!}
                         {!! $errors->first('password', '<span class="help-block">:message</span>') !!}
                        
                     </div>
-               <div class="row">
-
-                        <div class="col-md-12">
-
-                            <!-- BEGIN FORM WIZARD WITH VALIDATION -->
-                            <form class="form-wizard form-horizontal" action="" method="POST" id="wizard">
-                                <!-- CSRF Token -->
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-
-                                <!-- first tab -->
-                                <h1>User </h1>
-
-                                <section>
-                                
-                                    <div class="form-group">
-                                        <label for="first_name" class="col-sm-2 control-label">First Name *</label>
-                                        <div class="col-sm-10">
-                                            <input id="first_name" name="first_name" type="text" placeholder="First Name" class="form-control required" value="{{{ Input::old('first_name') }}}" />
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="last_name" class="col-sm-2 control-label">Last Name *</label>
-                                        <div class="col-sm-10">
-                                            <input id="last_name" name="last_name" type="text" placeholder="Last Name" class="form-control required" value="{{{ Input::old('last_name') }}}" />
-                                        </div>
-                                    </div>
-                                
-                                    <div class="form-group">
-                                        <label for="email" class="col-sm-2 control-label">Email *</label>
-                                        <div class="col-sm-10">
-                                            <input id="email" name="email" placeholder="E-Mail" type="text" class="form-control required email" value="{{{ Input::old('email') }}}" />
-                                        </div>
-                                    </div>
-                                
-                                    <div class="form-group">
-                                        <label for="password" class="col-sm-2 control-label">Password *</label>
-                                        <div class="col-sm-10">
-                                            <input id="password" name="password" type="password" placeholder="Password" class="form-control required" value="{{{ Input::old('password') }}}" />
-                                        </div>
-                                    </div>
-                                
-                                    <div class="form-group">
-                                        <label for="password_confirm" class="col-sm-2 control-label">Confirm Password *</label>
-                                        <div class="col-sm-10">
-                                            <input id="password_confirm" name="password_confirm" type="password" placeholder="Confirm Password " class="form-control required" value="{{{ Input::old('password_confirm') }}}" />
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="email" class="col-sm-2 control-label">Gender</label>
-                                        <div class="col-sm-10">
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form" action="" method="POST">
+                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                  <div class="box-body">
+                    <div class="form-group">
+                      <label for="first_name">First Name *</label>
+                      <input type="text" name="first_name" class="form-control" id="first_name" placeholder="First Name" value="{{{ Input::old('first_name') }}}">
+                    </div>
+                    <div class="form-group">
+                      <label for="last_name">Last Name *</label>
+                      <input type="text" class="form-control" id="last_name" placeholder="Last Name" name="last_name" value="{{{ Input::old('last_name') }}}">
+                    </div>
+                    <div class="form-group">
+                      <label for="email">Email *</label>
+                      <input type="text" class="form-control" id="email" placeholder="Email" name="email" value="{{{ Input::old('email') }}}">
+                    </div>
+                    <div class="form-group">
+                      <label for="last_name">Password *</label>
+                      <input type="text" class="form-control" id="password" placeholder="Password" name="password">
+                    </div>
+                    <div class="form-group">
+                      <label for="last_name">Confirm Password *</label>
+                      <input type="text" class="form-control" id="confirm_password" placeholder="Confirm Password" name="confirm_password">
+                    </div>
+                    <div class="dropdown">
+                                        <label for="gender" >Gender</label>
+                       
                                             <select class="form-control" title="Select Gender..." name="gender">
                                                 <option value="">Select</option>
                                                 <option value="male" @if(Input::old('gender') === 'male') selected="selected" @endif >MALE</option>
                                                 <option value="female" @if(Input::old('gender') === 'female') selected="selected" @endif >FEMALE</option>
 
                                             </select>
-                                        </div>
                                     </div>
-                                     <div class="form-group">
-                                        <label for="city" class="col-sm-2 control-label">City</label>
-                                        <div class="col-sm-10">
-                                            <input id="city" name="city" type="text" class="form-control" value="{{{ Input::old('city') }}}" />
-                                        </div>
-                                    </div>
+                    <div class="form-group">
+                      <label for="city">City</label>
+                      <input type="text" class="form-control" id="city" placeholder="City" name="city">
+                    </div>
+                    <div class="form-group">
+                      <label for="last_name">Address</label>
+                      <input type="text" class="form-control" id="address" placeholder="Address" name="address">
+                    </div>
+                  </div><!-- /.box-body -->
 
-                                    <div class="form-group">
-                                        <label for="address" class="col-sm-2 control-label">Address</label>
-                                        <div class="col-sm-10">
-                                            <input id="address" name="address" type="text" class="form-control" value="{{{ Input::old('address') }}}" />
-                                        </div>
-                                    </div>
-                                      <input type="submit" id="add_test" class="btn btn-md btn-primary" value="Add User" style="float:right;" />
-                              </section>
-                            </div>
-                            </div>
-                                    </form>
-            </div><!-- /.box-body -->
+                  <div class="box-footer">
+                    <input type="submit" class="btn btn-primary" value="Submit">
+                  </div>
+                </form>
+              </div> 
+
             <!--<div class="box-footer">
               Footer
             </div>--><!-- /.box-footer-->
-          </div><!-- /.box -->
+          </div>
+          <!-- /.box -->
 
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
