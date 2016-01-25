38d902ff97f82a30d4e9cb3c6016f0387e00b577
jhassan <jawedjee0519@gmail.com>
2016-01-25 23:50:37 +0500

    Log In

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,24 +13,40 @@
 
 /* ======================== ADMIN ROUTES ============================== */
 
-Route::get('/admin/', 'WelcomeController@index');
+/*Route::get('/admin/', function(){
+	if(Auth::guest()) { //return 'here';
+		return Redirect::to('auth/login');	
+	} else {
+		return Redirect::to('admin/users');		
+	}
+	
+});*/
+Route::filter('Sentry', function()
+{
+	if ( Auth::guest()) {
+ 		return Redirect::to('auth/login')->with('error', 'You must be logged in!');
+ 	}
+});
 
 Route::get('admin/product/index', 'ProductController@index');
 
 Route::get('admin/home', 'HomeController@index');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::get('index', function()
 {
     return View::make('admin/index');
 });
 
 Route::group(
-	array('prefix' => 'admin/users','before' => 'Sentry'), function () {
+	array('prefix' => 'admin','before' => 'Sentry'), function () {
 		Route::get('add', array('as' => 'add/user', 'uses' => 'UsersController@addUsers'));
         Route::get('/', array('as' => 'users', 'uses' => 'UsersController@listUers'));
 		Route::post('add', 'UsersController@createUser');
